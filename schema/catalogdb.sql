61e49c804c6bc79ddb6a4dd00e110814354fcb96
Conor Sayres <csayres@uw.edu>
2018-03-06 14:59:57 -0800

    fix index lines

--- schema/catalogdb.sql
+++ schema/catalogdb.sql
@@ -258,28 +258,28 @@ CREATE TABLE catalogdb.gaia_dr1_tgas (
 
 
 -- Indices
-CREATE INDEX CONCURRENTLY ra ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY dec ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY l ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY b ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY ecl_lon ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY ecl_lat ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY phot_g_mean_flux ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY phot_g_mean_mag ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY solution_id ON catalogdb.gaia_dr1_source using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY source_id ON catalogdb.gaia_dr1_source using BTREE(file_pk);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_ra_index ON catalogdb.gaia_dr1_source using BTREE(ra);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_dec_index ON catalogdb.gaia_dr1_source using BTREE(dec);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_l_index ON catalogdb.gaia_dr1_source using BTREE(l);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_b_index ON catalogdb.gaia_dr1_source using BTREE(b);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_ecl_lon_index ON catalogdb.gaia_dr1_source using BTREE(ecl_lon);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_ecl_lat_index ON catalogdb.gaia_dr1_source using BTREE(ecl_lat);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_phot_g_mean_flux_index ON catalogdb.gaia_dr1_source using BTREE(phot_g_mean_flux);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_phot_g_mean_mag_index ON catalogdb.gaia_dr1_source using BTREE(phot_g_mean_mag);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_solution_id_index ON catalogdb.gaia_dr1_source using BTREE(solution_id);
+CREATE INDEX CONCURRENTLY gaia_dr1_source_source_id_index ON catalogdb.gaia_dr1_source using BTREE(source_id);
 
-CREATE INDEX CONCURRENTLY ra ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY dec ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY l ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY b ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY ecl_lon ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY ecl_lat ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY phot_g_mean_flux ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY phot_g_mean_mag ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY solution_id ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY source_id ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY hip ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY tycho2_id ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
-CREATE INDEX CONCURRENTLY duplicated_source ON catalogdb.gaia_dr1_tgas using BTREE(file_pk);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_ra_index ON catalogdb.gaia_dr1_tgas using BTREE(ra);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_dec_index ON catalogdb.gaia_dr1_tgas using BTREE(dec);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_l_index ON catalogdb.gaia_dr1_tgas using BTREE(l);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_b_index ON catalogdb.gaia_dr1_tgas using BTREE(b);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_ecl_lon_index ON catalogdb.gaia_dr1_tgas using BTREE(ecl_lon);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_ecl_lat_index ON catalogdb.gaia_dr1_tgas using BTREE(ecl_lat);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_phot_g_mean_flux_index ON catalogdb.gaia_dr1_tgas using BTREE(phot_g_mean_flux);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_phot_g_mean_mag_index ON catalogdb.gaia_dr1_tgas using BTREE(phot_g_mean_mag);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_solution_id_index ON catalogdb.gaia_dr1_tgas using BTREE(solution_id);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_source_id_index ON catalogdb.gaia_dr1_tgas using BTREE(source_id);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_hip_index ON catalogdb.gaia_dr1_tgas using BTREE(hip);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_tycho2_id_index ON catalogdb.gaia_dr1_tgas using BTREE(tycho2_id);
+CREATE INDEX CONCURRENTLY gaia_dr1_tgas_duplicated_source_index ON catalogdb.gaia_dr1_tgas using BTREE(duplicated_source);
 
