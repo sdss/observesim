a52f40f32d04815304bdc580a8c79e2cdd006441
Conor Sayres <csayres@uw.edu>
2018-03-09 09:50:36 -0800

    some reorgs of schema dir

--- schema/catalogdb/gaia_dr1.sql
+++ schema/catalogdb/gaia_dr1.sql
@@ -1,8 +1,6 @@
 /*
 
-catalogDB schema version 0.0.1
-
-Created 2018 March 2 - C. Sayres
+schema for gaia dr1 tables
 
 types of each column from fits file.
 
@@ -133,10 +131,6 @@ drop schema catalogdb cascade;
 */
 
 
-CREATE SCHEMA catalogdb;
-
-SET search_path TO catalogdb;
-
 CREATE TABLE catalogdb.gaia_dr1_source (
     solution_id BIGINT,
     source_id BIGINT,
@@ -258,30 +252,3 @@ CREATE TABLE catalogdb.gaia_dr1_tgas (
     ecl_lat DOUBLE PRECISION);
 
 
--- Indices
-/*
-CREATE INDEX CONCURRENTLY gaia_dr1_source_ra_index ON catalogdb.gaia_dr1_source using BTREE(ra);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_dec_index ON catalogdb.gaia_dr1_source using BTREE(dec);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_l_index ON catalogdb.gaia_dr1_source using BTREE(l);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_b_index ON catalogdb.gaia_dr1_source using BTREE(b);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_ecl_lon_index ON catalogdb.gaia_dr1_source using BTREE(ecl_lon);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_ecl_lat_index ON catalogdb.gaia_dr1_source using BTREE(ecl_lat);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_phot_g_mean_flux_index ON catalogdb.gaia_dr1_source using BTREE(phot_g_mean_flux);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_phot_g_mean_mag_index ON catalogdb.gaia_dr1_source using BTREE(phot_g_mean_mag);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_solution_id_index ON catalogdb.gaia_dr1_source using BTREE(solution_id);
-CREATE INDEX CONCURRENTLY gaia_dr1_source_source_id_index ON catalogdb.gaia_dr1_source using BTREE(source_id);
-
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_ra_index ON catalogdb.gaia_dr1_tgas using BTREE(ra);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_dec_index ON catalogdb.gaia_dr1_tgas using BTREE(dec);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_l_index ON catalogdb.gaia_dr1_tgas using BTREE(l);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_b_index ON catalogdb.gaia_dr1_tgas using BTREE(b);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_ecl_lon_index ON catalogdb.gaia_dr1_tgas using BTREE(ecl_lon);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_ecl_lat_index ON catalogdb.gaia_dr1_tgas using BTREE(ecl_lat);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_phot_g_mean_flux_index ON catalogdb.gaia_dr1_tgas using BTREE(phot_g_mean_flux);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_phot_g_mean_mag_index ON catalogdb.gaia_dr1_tgas using BTREE(phot_g_mean_mag);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_solution_id_index ON catalogdb.gaia_dr1_tgas using BTREE(solution_id);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_source_id_index ON catalogdb.gaia_dr1_tgas using BTREE(source_id);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_hip_index ON catalogdb.gaia_dr1_tgas using BTREE(hip);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_tycho2_id_index ON catalogdb.gaia_dr1_tgas using BTREE(tycho2_id);
-CREATE INDEX CONCURRENTLY gaia_dr1_tgas_duplicated_source_index ON catalogdb.gaia_dr1_tgas using BTREE(duplicated_source);
-*/
---
+++ schema/catalogdb/tycho2.sql
@@ -0,0 +1,12 @@
+/*
+
+schema for tycho2 table
+
+http://tdc-www.harvard.edu/catalogs/tycho2.html
+http://tdc-www.harvard.edu/catalogs/tycho2.format.html#catalog
+
+*/
+
+CREATE TABLE catalogdb.tycho_two (
+
+) WITHOUT OIDS;
