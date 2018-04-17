5352d01e152a1d1d8df3002a3e71d45560dbed58
José Sánchez-Gallego <gallegoj@uw.edu>
2018-04-16 23:25:05 -0700

    Added generate_mock_targets and assign_targets_draining functions

--- python/observesim/test/test_utils.py
+++ python/observesim/test/test_utils.py
@@ -10,8 +10,9 @@
 
 import numpy as np
 import pytest
+from scipy.spatial.distance import cdist
 
-from ..utils import tp2xy, xy2tp
+from .. import utils
 
 
 class TestThetaPhi(object):
@@ -24,9 +25,9 @@ class TestThetaPhi(object):
         thetas = np.random.sample(n_actuators) * 360.
         phis = np.random.sample(n_actuators) * 360.
 
-        xy = tp2xy(thetas, phis)
+        xy = utils.tp2xy(thetas, phis)
 
-        theta_phi_recovered = xy2tp(xy[:, 0], xy[:, 1])
+        theta_phi_recovered = utils.xy2tp(xy[:, 0], xy[:, 1])
 
         for ii in range(n_actuators):
 
@@ -41,8 +42,8 @@ class TestThetaPhi(object):
             assert solution1_comp or solution2_comp
 
             # Checks that both solutions recover the (x, y) pair.
-            xy_from_solution1 = tp2xy(solutions[0, 0], solutions[0, 1])
-            xy_from_solution2 = tp2xy(solutions[1, 0], solutions[1, 1])
+            xy_from_solution1 = utils.tp2xy(solutions[0, 0], solutions[0, 1])
+            xy_from_solution2 = utils.tp2xy(solutions[1, 0], solutions[1, 1])
 
             assert pytest.approx(xy_from_solution1[0]) == xy[ii][0]
             assert pytest.approx(xy_from_solution1[1]) == xy[ii][1]
@@ -54,18 +55,55 @@ class TestThetaPhi(object):
         """Tests the utils.xy2tp function with invalid points."""
 
         # A point that is inside the inner ring
-        assert np.all(np.isnan(xy2tp(0, 0)))
+        assert np.all(np.isnan(utils.xy2tp(0, 0)))
 
         # A point that is not reachable
-        assert np.all(np.isnan(xy2tp(100, 100)))
+        assert np.all(np.isnan(utils.xy2tp(100, 100)))
 
     def test_xy2tp_phi_zero(self):
         """Tests the utils.xy2tp function when phi=0."""
 
-        results = xy2tp(22.4, 0., r_alpha=7.4, r_beta=15.0)
+        results = utils.xy2tp(22.4, 0., r_alpha=7.4, r_beta=15.0)
 
         assert pytest.approx(results[0, 0, 0]) == 0.
         assert pytest.approx(results[1, 0, 0]) == 0.
 
         assert pytest.approx(results[0, 0, 1], abs=1e-5) == 0.
         assert pytest.approx(results[1, 0, 1], abs=1e-5) == 360.
+
+
+class TestTargetAllocation(object):
+
+    def test_generate_mock_targets(self, robot):
+
+        targets = utils.generate_mock_targets(robot, min_distance=3)
+
+        assert len(targets) == (len(robot.xcen) - np.sum(robot.fiducial))
+
+        distances = cdist(targets, targets, 'euclidean')
+        distances_triu = distances[np.triu_indices(distances.shape[0], 1)]
+        assert np.all(distances_triu > 3)
+
+    def test_assign_targets_draining(self, robot):
+
+        # Creates a long list of targets
+        targets = np.vstack([utils.generate_mock_targets(robot, one_per_positioner=False)
+                             for __ in range(5)])
+
+        positioner_to_targets, target_to_positioners = utils.assign_targets_draining(
+            robot, targets, return_target_to_positioners=True)
+
+        assert len(positioner_to_targets) == len(robot.xcen) - np.sum(robot.fiducial)
+
+        for positioners in target_to_positioners.values():
+            assert len(positioners) > 0
+
+        # Checks that each positioner has the shortest possible list of targets
+        for positioner in positioner_to_targets:
+            assigned_targets = positioner_to_targets[positioner]
+            for target in assigned_targets:
+                valid_positioners = target_to_positioners[target]
+                assert positioner in valid_positioners
+                for valid_positioner in valid_positioners:
+                    n_target_valid_positioner = len(positioner_to_targets[valid_positioner])
+                    assert len(assigned_targets) <= (n_target_valid_positioner + 1)
--- python/observesim/utils.py
+++ python/observesim/utils.py
@@ -11,7 +11,8 @@
 import numpy as np
 
 
-__all__ = ['xy2tp', 'tp2xy']
+__all__ = ['xy2tp', 'tp2xy', 'generate_mock_targets',
+           'assign_targets_draining']
 
 
 def xy2tp(x, y, r_alpha=7.4, r_beta=15.0):
@@ -98,3 +99,172 @@ def tp2xy(theta, phi, r_alpha=7.4, r_beta=15.0):
     y = r_alpha * np.sin(np.radians(theta)) + r_beta * np.sin(np.radians(phi + theta))
 
     return np.array([x, y]).T
+
+
+def generate_mock_targets(robot, min_distance=3, one_per_positioner=True):
+    """An function to generate fully allocatable sets of targets.
+
+    Generates a list of ``(x, y)`` positions on the focal plane ensuring that
+    every actuator has a target within reach and that the targets are separated
+    a ``min_distance``.
+
+    Parameters:
+        robot (`~observesim.robot.Robot` object):
+            The `~observesim.robot.Robot` layout and geometry.
+        min_distance (float):
+            The minimum distance, in millimetres, that two targets can be
+            separated.
+        one_per_positioner (bool):
+            If ``True``, ensures that each positioner has an allocated target
+            (although a target may be in reach of multiple positioners).
+            Otherwise, targets are distributed randomly but always at a
+            position on the focal plane where at least a positioner can reach
+            them.
+
+    Returns:
+        targets (`~numpy.ndarray`):
+            A Numpy array with the ``(x, y)`` position on the focal plane. The
+            ordering is such that it matches the order of the actuators in
+            ``robot``.
+
+    """
+
+    positioners = np.array([robot.xcen[~robot.fiducial],
+                            robot.ycen[~robot.fiducial]]).T
+    n_positioners = positioners.shape[0]
+
+    targets = np.zeros(positioners.shape)
+
+    for nn in range(n_positioners):
+
+        valid = False
+
+        while not valid:
+
+            # Determines whether the new target will be placed around a
+            # positioner in sequential or random order.
+            if one_per_positioner:
+                positioner = positioners[nn, :]
+            else:
+                positioner = positioners[np.random.randint(n_positioners)]
+
+            new_target_tp = np.random.uniform(0, 360, size=2)
+            new_target_xy = tp2xy(new_target_tp[0], new_target_tp[1],
+                                  r_alpha=robot._ralpha, r_beta=robot._rbeta) + positioner
+
+            current_targets = targets[:nn]
+
+            if len(current_targets) > 0:
+                distances = np.sqrt(np.sum((current_targets - new_target_xy)**2, axis=1))
+                if np.any(distances <= min_distance):
+                    continue
+
+            valid = True
+            targets[nn, :] = new_target_xy
+
+    return targets
+
+
+def assign_targets_draining(robot, targets, return_target_to_positioners=False):
+    """Implements the target-fibre allocation method by Morales et al. (2012)
+
+    Uses a draining algorithm to optimally assign targets to
+    fibres/positioners. The method implements the following steps:
+
+    i) For each target, creates a list of available positioners.
+    ii) Creates a list of positioners. Assigns each target to the valid
+        positioner that has a shorter list of targets.
+    iii) Moves targets between positioners (as permitted by the lists defined
+         in ``(i)`` until all list are as short as possible.
+
+    Parameters:
+        robot (`~observesim.robot.Robot` object):
+            The `~observesim.robot.Robot` layout and geometry.
+        targets (`numpy.ndarray`):
+            An ``Nx2`` array with the ``(x, y)`` positions of the ``N`` targets
+            on the focal plane (in mm).
+        return_target_to_positioners (bool):
+            If ``True``, also returns a dictionary of indices from with valid
+            positioners for each target in ``targets``.
+
+    Returns:
+        indices (dict):
+            A dictionary in which the keys are the indices of the actuators in
+            ``robot``. For each element, the value is a list on the ``target``
+            indices associated to that actuator.
+
+    Example:
+        ::
+
+            >> from observesim.robot import Robot
+            >> from observesim.utils import assign_targets_draining, generate_mock_targets
+            >> robot = Robot()
+            >> targets = generate_mock_targets(robot)
+            >> assignment = assign_targets_draining(robot, targets)
+            >> print(assignment)
+            {0: [72], 1: [15], ...}
+            >> actuator_0_xcen = robot.xcen[0]
+            >> targets_for_actuator_0 = [targets[ii, :] for ii in assignment[0]]
+
+    """
+
+    targets = np.atleast_2d(targets)
+    assert targets.shape[1] == 2, 'invalid dimensions for target array.'
+
+    pos_idx = np.where(~robot.fiducial)[0]
+    pos_xy = np.array([robot.xcen[pos_idx], robot.ycen[pos_idx]]).T
+
+    target_to_positioners = {}
+    positioner_to_targets = {ii: [] for ii in pos_idx}
+    for target_idx in range(targets.shape[0]):
+
+        # Determines which actuators have a (theta, phi) configuration that
+        # would allow them to reach the target
+        target_xy = targets[target_idx, :]
+        target_distance = target_xy - pos_xy
+        theta_phi = xy2tp(target_distance[:, 0], target_distance[:, 1],
+                          r_alpha=robot._ralpha, r_beta=robot._rbeta)
+
+        # Valid positioners are those that have (theta, phi) not NaN
+        valid_positioners = np.unique(np.where(~np.isnan(theta_phi))[1])
+
+        # valid_positioners are indices of pos_xy. We need to convert them
+        # to pos_idx values.
+        valid_positioner_idx = [pos_idx[ii] for ii in valid_positioners]
+
+        # Assigns the list of valid positioners for this target
+        target_to_positioners[target_idx] = valid_positioner_idx
+
+        # Selects the valid positioner with fewer targets and assigns this
+        # target to it.
+        n_target_in_valid_positioners = [len(positioner_to_targets[ii])
+                                         for ii in valid_positioner_idx]
+
+        min_positioner = valid_positioner_idx[np.argmin(n_target_in_valid_positioners)]
+        positioner_to_targets[min_positioner].append(target_idx)
+
+    # Implements step (iii), reassigning targets to positioners until all
+    # lists are optimally short.
+
+    changed = True
+    while changed is True:
+
+        changed = False
+        new_positioner_to_targets = positioner_to_targets.copy()
+
+        for positioner in positioner_to_targets:
+            for target in positioner_to_targets[positioner]:
+                valid_positioners = target_to_positioners[target]
+                for new_positioner in valid_positioners:
+                    origin_length = len(new_positioner_to_targets[positioner])
+                    dest_length = len(new_positioner_to_targets[new_positioner])
+                    if dest_length + 1 < origin_length:
+                        new_positioner_to_targets[new_positioner] += [target]
+                        new_positioner_to_targets[positioner].remove(target)
+                        changed = True
+                        break
+
+    if return_target_to_positioners is False:
+        return positioner_to_targets
+    else:
+        return positioner_to_targets, target_to_positioners
