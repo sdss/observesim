423c5df3593d2b8ba010353bebb2d1bf4e178ca6
johndonor3 <j.donor@tcu.edu>
2019-07-25 10:19:50 -0500

    added variable exposure time based on airmass

--- python/observesim/observe.py
+++ python/observesim/observe.py
@@ -20,14 +20,36 @@ class Observe(object):
     result() : return result of an observation
 
 """
-    def __init__(self):
+    def __init__(self, defaultExp=None, cadencelist=None, cadences=None):
+        """Return simulated result of an observation
+
+        Parameters:
+        ----------
+
+        defaultExp : float
+            default exposure length in days
+
+        cadencelist : 
+            the cadencelist object from scheduler
+
+        cadences : 
+            the cadences from scheduler (of length fields, matching that index)
+        """
+
         self._result_dtype = [('sn2', np.float32),
                               ('mjd', np.float64),
                               ('duration', np.float64)]
         self._result0 = np.zeros(1, dtype=self._result_dtype)
+        self.cadencelist = cadencelist
+        self.cadences = cadences
+        if defaultExp is None:
+            self.defaultExp = np.float32(15. / 60. / 24.)
+        else:
+            self.defaultExp = defaultExp
+        self.defaultSN2 = 3000.
         pass
 
-    def result(self, fieldid=None, duration=None, mjd=None):
+    def result(self, fieldid=None, mjd=None, airmass=1, **kwargs):
         """Return simulated result of an observation
 
         Parameters:
@@ -36,11 +58,14 @@ class Observe(object):
         fieldid : int, np.int32
             id of field to observe
 
-        duration : float, np.float64
-            duration of observation (days)
-
         mjd : float, np.float64
             MJD start time of observation (days)
+
+        airmass: float, np.float64
+            The airmass of the observation
+
+        **kwargs
+            catch old duration kwarg quietly?
 
         Returns:
         -------
@@ -48,8 +73,21 @@ class Observe(object):
         result : dict
             has keys 'sn2', 'mjd', 'duration'
 """
+        
+        if self.cadences is None or self.cadencelist is None:
+            duration = self.defaultExp
+            sn2 = self.defaultSN2
+        else:
+            boss = self.cadencelist[self.cadences[fieldid]].requires_boss
+            if boss:
+                weight = 1
+            else:
+                weight = 0.05
+            duration = self.defaultExp * airmass ** weight
+            sn2 = self.defaultSN2  # + (np.random.randn() * 1000)
+
         fresult = self._result0
-        fresult['sn2'] = 3000.
+        fresult['sn2'] = sn2
         fresult['mjd'] = mjd
         fresult['duration'] = duration
         return(fresult)
