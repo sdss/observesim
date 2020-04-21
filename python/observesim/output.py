8a09e4d1e70a2c7fb8af2bc9f7def613fbfa917a
johndonor3 <j.donor@tcu.edu>
2020-04-21 14:57:05 -0600

    changes for old matplotlib, and move default cad_reqs file

--- python/observesim/output.py
+++ python/observesim/output.py
@@ -9,7 +9,7 @@ import yaml
 def read_field(field_id, exp_to_mjd, assign):
     # fetch info, match mjd to exp
     field_targs = assign[np.where(assign["fieldid"] == field_id)]
-    
+
     if len(np.unique(field_targs["exposure"])) > len(exp_to_mjd):
         exp_to_mjd.extend([-1 for i in range(len(np.unique(field_targs["exposure"])) - len(exp_to_mjd))])
 
@@ -27,9 +27,9 @@ def countFields(res_base, rs_base, plan, version=None, loc="apo", N=0, save=True
 
     assign = fitsio.read(rs_base + "{plan}/rsAssignments-{plan}-{loc}.fits".format(plan=plan, loc=loc))
     assign = assign[np.where(assign["pk"] != -1)]
-    
+
     allocation = fitsio.read(rs_base + "{plan}/rsAllocation-{plan}-{loc}.fits".format(plan=plan, loc=loc))
-        
+
     sim_data = fitsio.read(v_base + "{plan}-{loc}-fields-{n}.fits".format(plan=plan, loc=loc, n=N))
     obs_data = fitsio.read(v_base + "{plan}-{loc}-observations-{n}.fits".format(plan=plan, loc=loc, n=N))
 
@@ -38,39 +38,39 @@ def countFields(res_base, rs_base, plan, version=None, loc="apo", N=0, save=True
     all_cads = list()
     all_mjds = list()
     all_fields = list()
-    
+
     start_time = time.time()
-    
+
     for f in sim_data:
         obs_idx = f["observations"][:int(f["nobservations"])]
         mjds = [obs_data[i]["mjd"] for i in obs_idx]
         cad = f["cadence"]
         real_fid = allocation[f["fieldid"]]["fieldid"]
-        
+
 #         if real_fid % 100 == 0:
 #             print(real_fid, (time.time() - start_time)/60.)
-        
+
         ids, cadences, targ_mjds = read_field(real_fid, mjds, assign)
         all_targs.extend(ids)
         all_cads.extend(cadences)
         all_mjds.extend(targ_mjds)
         all_fields.extend([real_fid for f in ids])
-    
+
     assert len(all_targs) == len(all_cads), "targ != cad!!"
     assert len(all_targs) == len(all_mjds), "targ != mjd!!"
     assert len(all_targs) == len(all_fields), "targ != field!!"
-    
+
     dtype = [('pk', np.int32),
              ('cadence', np.dtype('a40')),
              ('field_id', np.int32),
              ('obs_mjd', np.float64)]
     obs_targs = np.zeros(len(all_targs), dtype=dtype)
-    
+
     obs_targs["pk"] = all_targs
     obs_targs["cadence"] = all_cads
     obs_targs["field_id"] = all_fields
     obs_targs["obs_mjd"] = all_mjds
-    
+
     if save:
         fitsio.write(v_base + "obsTargets-{plan}-{loc}.fits".format(plan=plan, loc=loc), obs_targs,
                  clobber=True)
@@ -99,18 +99,18 @@ The fields files contain indices into an observations to record info for each ob
 target_table = """<h2>Summary of Observed Targets </h2>
 <p>For each observation of a field, the targets planned to be observed are specified in an
 rsAssignments file. The targets can therefor be matched to field observations. The results of
-this matching are here for <a href="obsTargets-{plan}-apo.fits">APO</a> 
+this matching are here for <a href="obsTargets-{plan}-apo.fits">APO</a>
 and <a href="obsTargets-{plan}-apo.fits">LCO</a>. They can then be sorted by cadence to determine how many
 targets where observed total. The plot below shows a summary of total targets per cadence, note the log
 scale.</p>
 
 <a href="{plan}-target_summary.pdf"><img src="{plan}-target_summary.png" width="900px/"> </a>
 
-<p>Below the total number of targets observed, as well as the number observed at each observatory, 
+<p>Below the total number of targets observed, as well as the number observed at each observatory,
 is shown. A csv file containing this information is also <a href="{plan}-target_summary.txt">available</a>.</p>
 
 <table><tbody>
-<tr><td><h4>Cadence</h4></td> <td><h4>required</h4></td> 
+<tr><td><h4>Cadence</h4></td> <td><h4>required</h4></td>
 <td><h4>total</h4></td> <td><h4>APO</h4></td>
 <td><h4>LCO</h4></td></tr>
 """
@@ -138,7 +138,7 @@ def writeWebPage(base, plan, version=None):
     else:
         v_base = os.path.join(base, plan)
         v_base += "/"
-    
+
     html = header + "\n" + bar_plots + "\n" + fits_out + "\n" + target_table + "\n"
     html = html.format(plan=plan)
 
@@ -149,7 +149,7 @@ def writeWebPage(base, plan, version=None):
     for t in targ_sum:
         html += targ_table_row.format(cad=t["cadence"], req=t["required"],
                                 total=t["total"], apo=t["apo"], lco=t["lco"])
-    
+
     html += "\n" + table_heads + "\n"
 
     files = os.listdir(v_base)
@@ -158,10 +158,10 @@ def writeWebPage(base, plan, version=None):
     apo = [f for f in cum_pngs if "apo" in f and "none" not in f]
     lco.sort()
     apo.sort()
-    
+
     len_l = len(lco)
     len_a = len(apo)
-    
+
     for i in range(max(len_l, len_a)):
         if i <= len_l - 1:
             l_file = lco[i]
@@ -172,7 +172,7 @@ def writeWebPage(base, plan, version=None):
         else:
             a_file = ""
         html += table_row.format(apo_png=a_file, lco_png=l_file) + "\n"
-    
+
     return html + tail
 
 
@@ -190,24 +190,24 @@ def getCounts(res_base, rs_base, plan, version=None, loc="apo"):
     for f in res_files:
         sim_data = fitsio.read(v_base + f)
         counts.append(sim_data['nobservations'])
-    
+
     fields = fitsio.read(rs_base+"{plan}/rsAllocation-{plan}-{loc}.fits".format(plan=plan, loc=loc))
-    
+
     planned = fields['needed']
-    
+
     counts = np.mean(np.array(counts), axis=0)
-    
-    return counts, planned, fields["cadence"]
+
+    return counts, planned, [c.decode() for c in fields["cadence"]]
 
 
 def tabulate(counts, planned, cadence):
     completion = dict()
     visits = dict()
     plan_count = dict()
-    
+
     print(counts)
     print(len(counts), len(planned), len(cadence))
-    
+
     for n, p, c in zip(counts, planned, cadence):
         if c in completion:
             completion[c].append(n/p)
@@ -242,19 +242,19 @@ def doHist(res_base, rs_base, plan, version=None, loc="apo", level=0.95):
     new_keys = [convertCadence(k) for k in orig_keys]
     sort_keys = np.sort(np.unique(new_keys))
     cad_to_indx = {k: i for i, k in enumerate(sort_keys)}
-    
+
     reduced_keys = {k: [] for k in sort_keys}
     reduced_vis = {k: [] for k in sort_keys}
     reduced_plan = {k: [] for k in sort_keys}
-    
+
     for key, v in completion.items():
         k = convertCadence(key)
         reduced_keys[k].extend(v)
         reduced_vis[k].extend(vis_count[key])
         reduced_plan[k].extend(plan_count[key])
-    
+
     plt.figure(figsize=(12, 12))
-    
+
     ax1 = plt.subplot(211)
     ax2 = plt.subplot(212)
 
@@ -266,7 +266,7 @@ def doHist(res_base, rs_base, plan, version=None, loc="apo", level=0.95):
 
     vis = list()
     planned = list()
-    
+
     for k, v in reduced_keys.items():
         x.append(cad_to_indx[k])
 
@@ -280,7 +280,7 @@ def doHist(res_base, rs_base, plan, version=None, loc="apo", level=0.95):
         vis.append(vis_sum)
         plan_sum = np.sum(reduced_plan[k])
         planned.append(plan_sum)
-    
+
     width = 0.3
 
     ax1.bar(np.array(x) - width, mean, width, color="b", label="mean")
@@ -291,27 +291,27 @@ def doHist(res_base, rs_base, plan, version=None, loc="apo", level=0.95):
     ax1.legend()
 
     ax3 = ax1.twiny()
-    
+
     labels = ax1.get_xticks()
     ax3.set_xlim(ax1.get_xlim())
     ax3.set_xticks(labels)
     ax3.set_xticklabels(total, rotation='vertical')
-    
-    ax2.bar(np.array(x) - width, vis, width, color="c", label="visits")
-    ax2.bar(np.array(x), planned, width, color="coral", label="planned visits")
+
+    ax2.bar(np.array(x) - width, vis, width, color="c", label="visits", log=True)
+    ax2.bar(np.array(x), planned, width, color="coral", label="planned visits", log=True)
     ax2.set_xticks(x, minor=False)
     ax2.set_xticklabels(label, rotation='vertical')
     ax2.set_ylabel("# visits")
     ax2.legend()
-    ax2.set_yscale("log")
+    # ax2.set_yscale("log")
 
     ax1.set_title("{}: {}".format(plan, loc))
-    
+
     plt.tight_layout()
     if version is None:
         version = plan
-    plt.savefig(os.path.join(base, version)+"/{}-{}-cadence_bar.pdf".format(plan, loc))
-    plt.savefig(os.path.join(base, version)+"/{}-{}-cadence_bar.png".format(plan, loc))
+    plt.savefig(os.path.join(res_base, version)+"/{}-{}-cadence_bar.pdf".format(plan, loc))
+    plt.savefig(os.path.join(res_base, version)+"/{}-{}-cadence_bar.png".format(plan, loc))
 
 
 def getEpochsFromFile(base, plan, version=None, loc="apo", N=0):
@@ -328,12 +328,12 @@ def getEpochsFromFile(base, plan, version=None, loc="apo", N=0):
     for f in sim_data:
         obs_idx = f["observations"][:int(f["nobservations"])]
         mjds = [obs_data[i]["mjd"] for i in obs_idx]
-        cad = f["cadence"]
+        cad = f["cadence"].decode()
         if cad in cad_mjds:
             cad_mjds[cad].extend(mjds)
         else:
             cad_mjds[cad] = mjds
-    
+
     cad_mjds = {k: np.array(v) for k, v in cad_mjds.items()}
 #         print(f["cadence"], np.min(mjds))
     return cad_mjds
@@ -346,12 +346,12 @@ def cumulativePlot(base, plan, version=None, loc="apo"):
     for k, i in cad_mjds.items():
         all_mjds.extend(list(i))
     min_mjd, max_mjd = int(np.min(all_mjds)), int(np.max(all_mjds))
-    
+
     sort_keys = np.sort(np.unique(new_cad))
-    
+
     mjds = np.arange(min_mjd, max_mjd, 1)
     plot_cads = {k: [] for k in sort_keys}
-    
+
     for m in mjds:
         today = dict()
         for k, v in cad_mjds.items():
@@ -369,28 +369,28 @@ def cumulativePlot(base, plan, version=None, loc="apo"):
     fig.set_size_inches(12, rows*1.5)
 
     cad_to_indx = {k: i for i, k in enumerate(sort_keys)}
-    
+
     flat_axes = axes.flatten()
 
     plt.title("{}-{}".format(plan, loc))
-    
+
     for k, v in plot_cads.items():
         ax = flat_axes[cad_to_indx[k]]
         ax.plot(mjds, v)
         ax.set_title(k)
-    
+
     for a in axes[:, 0]:
         a.set_ylabel("# visits", fontsize=16)
-    
+
     axes[-1, 0].set_xlabel("mjd", fontsize=16)
     axes[-1, 1].set_xlabel("mjd", fontsize=16)
     plt.tight_layout()
     if version is None:
         version = plan
     plt.savefig(os.path.join(base, version)+"/{}-{}-cumulative.pdf".format(plan, loc))
-    
+
     plt.close()
-    
+
     for k, v in plot_cads.items():
         plt.figure(figsize=(8,5))
         ax = plt.subplot(111)
@@ -410,7 +410,7 @@ def passesCadence(targ_mjds, cadence=None):
             # dumb for now, get back to this
             return True
 
-def plotTargMetric(v_base, plan, version=None, reqs_file=None):
+def plotTargMetric(base, plan, version=None, reqs_file=None):
     if version is not None:
         v_base = os.path.join(base, version)
         v_base += "/"
@@ -423,18 +423,18 @@ def plotTargMetric(v_base, plan, version=None, reqs_file=None):
     lco_targs_mjds = {i: [] for i in np.unique(lco_targs["pk"])}
     apo_targs_mjds = {i: [] for i in np.unique(apo_targs["pk"])}
     targ_to_cad = dict()
-    
+
     for t in lco_targs:
         lco_targs_mjds[t["pk"]].append(t["obs_mjd"])
-        targ_to_cad[t["pk"]] = t["cadence"]
-    
+        targ_to_cad[t["pk"]] = t["cadence"].decode()
+
     for t in apo_targs:
         apo_targs_mjds[t["pk"]].append(t["obs_mjd"])
-        targ_to_cad[t["pk"]] = t["cadence"]
-    
+        targ_to_cad[t["pk"]] = t["cadence"].decode()
+
     # #####################
     # #####################
-    
+
     all_cads = np.sort(np.unique([v for k, v in targ_to_cad.items()]))
     lco_cads = {c: [] for c in all_cads}
     apo_cads = {c: [] for c in all_cads}
@@ -446,43 +446,44 @@ def plotTargMetric(v_base, plan, version=None, reqs_file=None):
     for t, v in apo_targs_mjds.items():
         if passesCadence(v):
             apo_cads[targ_to_cad[t]].append(t)
-    
+
     # #####################
     # #####################
 
     plt.figure(figsize=(16, 10))
-    
+
     ax1 = plt.subplot(111)
-    
+
     apo_counts = [len(v) for k, v in apo_cads.items()]
     lco_counts = [len(v) for k, v in lco_cads.items()]
     names = [k.strip() for k in apo_cads.keys()] # same as lco_cad.keys()
     x = range(len(names))
 
-    ax1.bar(x, apo_counts, 0.4, color="b", label="apo")
-    ax1.bar(x, lco_counts, 0.4, bottom=apo_counts, color="r", label="lco")
+
+    ax1.bar(x, lco_counts, color="c", label="lco", log=True)
+    ax1.bar(x, apo_counts, bottom=lco_counts, color="r", label="apo", log=True)
     ax1.set_xticks(x, minor=False)
     ax1.set_xticklabels(names, rotation='vertical')
     ax1.set_ylabel("# targs")
-    ax1.set_yscale("log")
-    
+    # ax1.set_yscale("log")
+
     ax3 = ax1.twiny()
-    
+
     labels = ax1.get_xticks()
     ax3.set_xlim(ax1.get_xlim())
     ax3.set_xticks(labels)
     ax3.set_xticklabels([x + y for x, y in zip(apo_counts, lco_counts)], rotation='vertical')
 
     ax1.legend()
-    
+
     if reqs_file is None:
-        reqs_file = '/'.join(os.path.realpath(__file__).split('/')[0:-2]) + "/etc/cadence_reqs.yml"
+        reqs_file = '/'.join(os.path.realpath(__file__).split('/')[0:-1]) + "/etc/cadence_reqs.yml"
 
     req_by_cad = yaml.load(open(reqs_file))
-    
+
     sum_text = "{cad:30s}, {req:9s}, {total:8s}, {apo:8s}, {lco:8s}\n".format(
                 cad="cadence", req="required", total="total", apo="apo", lco="lco")
-    
+
     for k in apo_cads.keys():
         apo = len(apo_cads[k])
         lco = len(lco_cads[k])
@@ -492,7 +493,7 @@ def plotTargMetric(v_base, plan, version=None, reqs_file=None):
             req = ""
         sum_text += "{cad:30s}, {req:9s}, {total:8d}, {apo:8d}, {lco:8d}\n".format(
                      cad=k.strip(), req=str(req), total=apo+lco, apo=apo, lco=lco)
-    
+
     res_base = v_base + plan
 
     plt.savefig(res_base + "-target_summary.pdf")
